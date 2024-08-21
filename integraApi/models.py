c229968549c34d7c8f2885786212714f476fa472
Laryssa Medeiros <67442535+Laryssa75@users.noreply.github.com>
2024-08-21 09:36:14 -0300

    deletando pasta duplicada

--- integraApi/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-#Models serve para armazenar ou alterar os dados do usuário enviados do aplicativo flutter para o banco
-
-class Funcionario(moodels.Model):
-    NOMEFUNC = models.CharField(max_length=20)
-    CODFUNC = models.IntegerField(max_length=10)
-    CPF = models.InterField(max_length=13)
