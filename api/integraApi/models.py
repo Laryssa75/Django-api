834cdf021cd233566c28de0c7e5de876c101c94a
Luiz Henrique <69431619+Hikari-no-Michi@users.noreply.github.com>
2024-10-22 11:45:23 -0300

    Update models.py

--- api/integraApi/models.py
+++ api/integraApi/models.py
@@ -1,11 +1,35 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
-#Models serve para armazenar ou alterar os dados do usuário enviados do aplicativo flutter para o banco
+class Coin(models.Model):
+    saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
-class Funcionario(models.Model):
-    class Meta:
-        db_table = 'TFPFUN'
-        managed = False
+    def atualizar_saldo(self, valor):
+        self.saldo += valor
+        self.save()
 
-    
+    @staticmethod
+    def get_saldo_atual():
+        coin, created = Coin.objects.get_or_create(id=1)  # Assume apenas uma instância
+        return coin
+
+class Entrada(models.Model):
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.CharField(max_length=255)
+    data = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        coin = Coin.get_saldo_atual()
+        coin.atualizar_saldo(self.valor)
+
+class Saída(models.Model):
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.CharField(max_length=255)
+    data = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        coin = Coin.get_saldo_atual()
+        coin.atualizar_saldo(-self.valor)
